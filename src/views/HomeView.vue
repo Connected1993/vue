399d81eb90039f83b45e85ce68a6c3ac097ddce0
Alex <alexanderbalabas1993@gmail.com>
2025-04-09 21:04:12 +0300

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="https://inordic.ru/assets/images/icons/logo.svg" height="100">
+    <HelloWorld msg="Welcome to version my-site-2"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
